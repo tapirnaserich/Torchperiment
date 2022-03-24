3c146a96edf709cb12b33260f647b14ebac08e7e
tapirnaserich <tobias.arndt@alumni.fh-aachen.de>
2022-03-24 10:37:36 +0100

    hyperparameter optimization: first steps

--- Tools/experiment_generator.py
+++ Tools/experiment_generator.py
@@ -10,6 +10,31 @@ from jinja2 import Template
 from Library.Generators.experiment_generation import experiment_generator
 from Library.utils import compile_template
 
+
+
+def generate_experiment():
+    template_path = f'{path}/Library/Templates/experiment_generate_template.jinja2'
+
+
+
+    args = args_parser.parse_args()
+    print(path)
+    experiment_path = f'{path}/{args.base_target}/{args.target}'
+    if not os.path.exists(experiment_path):
+        os.mkdir(experiment_path)
+
+    module = experiment_generator(base_path=args.base_path, path=args.path)
+    setattr(args, 'params', module.dependencies)
+
+    template = compile_template(template_path=template_path,
+                                data=args)
+
+    with open(f'{experiment_path}/settings.py', "w") as fh:
+        fh.write(template)
+
+
+
+
 if __name__ == '__main__':
 
     template_path =f'{path}/Library/Templates/experiment_generate_template.jinja2'
@@ -20,7 +45,7 @@ if __name__ == '__main__':
                              default='Classes.Factory')
     args_parser.add_argument('-p', '--path',
                              action='store', type=str,
-                             default='factory_autoencoder')
+                             default='factory_mnist')
 
     args_parser.add_argument('-bt', '--base_target',
                                   action='store', type=str,
